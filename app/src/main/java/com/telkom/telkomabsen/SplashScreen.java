8eedcc1aca7e667c1754260371caf646b87be1cf
Hilmi Zaidan <hilmizaidan@users.noreply.github.com>
2020-08-26 10:13:29 +0700

    test 2

--- app/src/main/java/com/telkom/telkomabsen/SplashScreen.java
+++ app/src/main/java/com/telkom/telkomabsen/SplashScreen.java
@@ -2,13 +2,25 @@ package com.telkom.telkomabsen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+    private int time_loading = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent splash = new Intent(SplashScreen.this,ActivityRegister.class);
+                startActivity(splash);
+                finish();
+            }
+        },time_loading);
     }
 }
